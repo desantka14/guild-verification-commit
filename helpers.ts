2ce7b4e86e94e1c6afb6541dd4dea8ae65207656
desantka14 <ebulfetehmedov14@gmail.com>
2026-04-01 18:35:37 +0400

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-01T14:35:34.208Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-01T14:35:35.921Z
+Commit: Add export functionality for reports
