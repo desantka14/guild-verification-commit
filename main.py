a22d0c443218f42e75f3f3a60ce24117a2d6ec9b
desantka14 <ebulfetehmedov14@gmail.com>
2026-04-01 18:35:53 +0400

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-01T14:35:32.284Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-01T14:35:52.649Z
+Commit: Resolve null pointer exception in user service
