8214834bffb924b6787246fc6893b97a4848fbac
desantka14 <ebulfetehmedov14@gmail.com>
2026-04-01 18:35:50 +0400

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-01T14:34:59.625Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-01T14:35:48.899Z
+Commit: Fix broken links in documentation
