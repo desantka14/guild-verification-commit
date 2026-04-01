cbba78215a6594f89bda0c401be4a16626dc5609
desantka14 <ebulfetehmedov14@gmail.com>
2026-04-01 18:35:35 +0400

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-01T14:35:23.075Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-01T14:35:34.208Z
+Commit: Patch API endpoint error handling
