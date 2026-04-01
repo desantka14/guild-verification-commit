104734c1de51f1d9cbea1652b0b09b26f62707d8
desantka14 <ebulfetehmedov14@gmail.com>
2026-04-01 18:34:58 +0400

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-01T14:34:54.100Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-01T14:34:57.418Z
+Commit: Resolve null pointer exception in user service
